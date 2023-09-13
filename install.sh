5c55e2f53d4667ea32fbb6cc947e5d36710284d4
Ahmad Rezaei <parsarezaei53@gmail.com>
2023-09-13 08:48:45 +0330

    Download link chnaged to arvan s3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://download.mikrotik.com/routeros/7.11.2/chr-7.11.2.img.zip -O chr.img.zip  && \
+wget https://manageit.s3.ir-thr-at1.arvanstorage.ir/chr-7.11.2.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 echo u > /proc/sysrq-trigger && \
 dd if=chr.img bs=1024 of=/dev/sda && \
